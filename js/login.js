ef847d6802f45d8f56c168d8b8978d9b82c2149d
DelanoKano <2042653@talnet.nl>
2025-06-16 23:24:56 +0200

    v3.0.1

--- js/login.js
+++ js/login.js
@@ -1,18 +1,27 @@
-document.getElementById('loginForm').addEventListener('submit', function (e) {
+document.getElementById("loginForm").addEventListener("submit", function (e) {
   e.preventDefault();
 
-  const username = document.getElementById('username').value.trim();
-  const password = document.getElementById('password').value.trim();
+  const username = document.getElementById("username").value.trim();
+  const password = document.getElementById("password").value.trim();
 
-  const users = JSON.parse(localStorage.getItem('users')) || [];
-  const user = users.find(u => u.username === username && u.password === password);
+  const users = JSON.parse(localStorage.getItem("users")) || [];
+  const user = users.find(
+    (u) => u.username === username && u.password === password
+  );
 
   if (!user) {
-    alert('Invalid username or password.');
+    alert("Invalid username or password.");
     return;
   }
 
-  localStorage.setItem('loggedInUser', JSON.stringify(user));
-  alert('Login successful!');
-  window.location.href = 'dashboard.html';
+  if (user.username === "Della") {
+    alert("Welcome, Admin!");
+
+    window.location.href = "Admin/admin.html";
+
+  } else {
+    localStorage.setItem("loggedInUser", JSON.stringify(user));
+    alert("Login successful!");
+    window.location.href = "dashboard.html";
+  }
 });
